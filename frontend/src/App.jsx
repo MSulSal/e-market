be3e89611384c80f5dd59f9da9d8be3220d3ca6d
MSulSal <msulsaleem@gmail.com>
2025-02-06 17:05:17 -0600

    add barebones of navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
